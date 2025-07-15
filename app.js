393da17761a5b89a4badfd96b2b278896f4f4b4f
Subash-08 <128483625+Subash-08@users.noreply.github.com>
2025-07-15 19:50:30 +0530

    first commit

--- app.js
+++ app.js
@@ -33,6 +33,10 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieParser());
+app.use((req, res, next) => {
+  console.log('Request:', req.method, req.url);
+  next();
+});
 
 // Routes
 app.use('/api/auth/', authRoutes);
